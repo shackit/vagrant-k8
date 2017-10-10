29ec5939310ff844c42c62d784ae67b8ea425144
Stephen Hackett <shackett@cloudiconic.com>
2017-10-10 15:07:04 +0100

    remove manual create cruft

--- init-master.sh
+++ init-master.sh
@@ -6,44 +6,24 @@ update_host master
 
 config_hosts_file
 
-sudo yum -y install etcd flannel kubernetes
+function install_etcd(){
+    # download the etcd release
+    wget -q --show-progress --https-only --timestamping \
+    "https://github.com/coreos/etcd/releases/download/v3.2.8/etcd-v3.2.8-linux-amd64.tar.gz"
 
-# Download HELM
-cd ~
-curl -L \
-https://kubernetes-helm.storage.googleapis.com/helm-v2.4.1-linux-amd64.tar.gz -O
-tar -xf helm-v2.4.1-linux-amd64.tar.gz
-sudo mv linux-amd64/helm /usr/local/bin/helm
+    # unpack the etcd release
+    tar -xvf etcd-v3.2.8-linux-amd64.tar.gz
 
-# Configure etcd:
-echo ETCD_NAME=default | sudo tee /etc/etcd/etcd.conf
-echo ETCD_DATA_DIR="/var/lib/etcd/default.etcd" | sudo tee -a /etc/etcd/etcd.conf
-echo ETCD_LISTEN_CLIENT_URLS="http://0.0.0.0:2379" | sudo tee -a /etc/etcd/etcd.conf
-echo ETCD_ADVERTISE_CLIENT_URLS="http://localhost:2379" | sudo tee -a /etc/etcd/etcd.conf
+    # make it accessible system wide
+    sudo mv etcd-v3.2.8-linux-amd64/etcd* /usr/local/bin/
+}
 
-# Configure kubernetes api server:
-echo KUBE_API_ADDRESS="--address=192.168.1.80" | sudo tee /etc/kubernetes/apiserver
-echo KUBE_API_PORT="--port=8080" | sudo tee -a /etc/kubernetes/apiserver
-echo KUBELET_PORT="--kubelet_port=10250" | sudo tee -a /etc/kubernetes/apiserver
-echo KUBE_ETCD_SERVERS="--etcd_servers=http://127.0.0.1:2379" | sudo tee -a /etc/kubernetes/apiserver
-echo KUBE_SERVICE_ADDRESSES="--service-cluster-ip-range=172.16.0.0/16" | sudo tee -a /etc/kubernetes/apiserver
-echo KUBE_ADMISSION_CONTROL="--admission_control=NamespaceLifecycle,NamespaceExists,LimitRanger,SecurityContextDeny,ResourceQuota" | sudo tee -a /etc/kubernetes/apiserver
+function config_etcd(){
+    # setup config directories
+    sudo mkdir -p /etc/etcd /var/lib/etcd
 
-sudo sed -i "s/KUBE_MASTER=\"--master=http:\/\/127.0.0.1:8080\"/KUBE_MASTER=\"--master=http:\/\/${MASTER_IP}:8080\"/g" /etc/kubernetes/config
-
-# Point flannel to Master IP:
-sudo sed -i "s/FLANNEL_ETCD_ENDPOINTS=\"http:\/\/127.0.0.1:2379\"/FLANNEL_ETCD_ENDPOINTS=\"http:\/\/${MASTER_IP}:2379\"/g" /etc/sysconfig/flanneld
-
-# Set public interface for flannel - not vagrant nat iface:
-# ToDo: This may fix auto discovry of api server
-iface=$(ip a | grep ${MASTER_IP} | cut -d ' ' -f 11)
-sudo sed -i "s/#FLANNEL_OPTIONS=\"\"/FLANNEL_OPTIONS=\"--iface=${iface}\"/g" /etc/sysconfig/flanneld
-
-for SVC in etcd kube-apiserver kube-controller-manager kube-scheduler flanneld; do
-  systemctl restart $SVC
-  systemctl enable $SVC
-  systemctl status $SVC
-done
-
-# Setup Flannel configuration in etcd:
-etcdctl set /atomic.io/network/config < /vagrant/flannel-config-vxlan.json
+    # copy self signed tls certs into place
+    sudo cp /vagrant/thw/certs/01_ca/ca.pem \
+    /vargrant/thw/05_kubernetes/kubernetes-key.pem \
+    /vargrant/thw/05_kubernetes/kubernetes.pem /etc/etcd/
+}
